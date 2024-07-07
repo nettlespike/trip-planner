65f88939178f7a146a36041455f061037f408563
Joey Shum <joeyshum8888@gmail.com>
2024-07-07 17:57:17 -0400

    Added delete confirmation dialog

--- client/src/pages/Pois.jsx
+++ client/src/pages/Pois.jsx
@@ -1,11 +1,42 @@
 import React from "react";
 import { useEffect } from "react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import Dialog from "./Dialog";
 
 const Pois = () => {
   const [pois, setPois] = useState([]);
+
+  const [dialog, setDialog] = useState({
+    message: "",
+    isLoading: false,
+    //Update
+    namePoi: ""
+  });
+  const idPoiRef = useRef();
+  const handleDialog = (message, isLoading, namePoi) => {
+    setDialog({
+      message,
+      isLoading,
+      //Update
+      namePoi
+    });
+  };
+
+  const handleDeleteReq = (id) => {
+    handleDialog("Are you sure you want to delete?", true);
+    idPoiRef.current = id;
+  };
+
+  const areUSureDelete = (choose) => {
+    if (choose) {
+      handleDelete(idPoiRef.current);
+      handleDialog("", false);
+    } else {
+      handleDialog("", false);
+    }
+  };
 
   useEffect(() => {
     const fetchAllPois = async () => {
@@ -47,7 +78,7 @@ const Pois = () => {
               <p>{poi.accessibility}</p>  
             </div>
             <div className="but">
-              <button className="delete" onClick={() => handleDelete(poi.pid)}>Delete</button>
+              <button className="delete" onClick={() => handleDeleteReq(poi.pid)}>Delete</button>
               <button className="update">
                 <Link to={`/update/${poi.pid}`} style={{ color: "inherit", textDecoration: "none" }}>
                   Update
@@ -57,6 +88,14 @@ const Pois = () => {
             
           </div>
         ))}
+        {dialog.isLoading && (
+        <Dialog
+          //Update
+          namePoi={dialog.namePoi}
+          onDialog={areUSureDelete}
+          message={dialog.message}
+        />
+        )}
       </div>
 
       <button className="addHome">
