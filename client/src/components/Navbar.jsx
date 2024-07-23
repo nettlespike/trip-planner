cf7f5e9f5c789f542f3599f27105b2c63710dc9a
Joey Shum <joeyshum8888@gmail.com>
2024-07-23 00:25:40 -0400

    Navbar adjusted for different types of users

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/authContext";
-// import Logo from "../img/logo.png";
 
 const Navbar = () => {
   const { currentUser, logout } = useContext(AuthContext);
@@ -10,17 +9,21 @@ const Navbar = () => {
     <div className="navbar">
       <div className="container">
         <div className="links">
-          {/* <Link className="link" to="/?cat=art"><h6>Filter1</h6></Link>
-          <Link className="link" to="/?cat=science"><h6>Filter2</h6></Link>
-          <Link className="link" to="/?cat=technology"><h6>Filter3</h6></Link> */}
           <span>{"Hello, "+ currentUser?.username}</span>
           {currentUser ? (
             <span onClick={logout}>Logout</span>
           ) : (
             <Link className="link" to="/login">Login</Link>
           )}
-          <span className="home"><Link className="home" to="/">Customer Home Page</Link></span>
+          {currentUser?.isAdmin ? 
+            <span className="home"><Link className="home" to="/">Customer Home Page</Link></span> : 
+            <span className="home"><Link className="home" to="/">Home Page</Link></span>
+          }
           <span className="write"><Link className="link" to="/review">View all reviews</Link></span>
+          {currentUser?.isAdmin ?
+            <span className="manage"><Link className="manage" to="/poi">Manage POIs</Link></span> : 
+            null
+          }
         </div>
       </div>
     </div>
