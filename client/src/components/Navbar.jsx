587e059076291060f18f024da1e52099519754a6
Ceciliaxia12 <y224xia@uwaterloo.ca>
2024-07-24 14:49:26 -0400

    Bold format for the page that we are on

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom"; // Added useLocation
 import { AuthContext } from "../context/authContext";
 
 const Navbar = () => {
   const { currentUser, logout } = useContext(AuthContext);
+  const location = useLocation(); // Added useLocation to get the current route
 
   return (
     <div className="navbar">
@@ -18,16 +19,16 @@ const Navbar = () => {
             <Link className="link" to="/login">Login</Link>
           )}
           {currentUser?.isAdmin ? 
-            <span className="home"><Link className="home" to="/">Customer Home Page</Link></span> : 
-            <span className="home"><Link className="home" to="/">Home Page</Link></span>
+            <span className="home"><Link className={location.pathname === '/' ? 'home active' : 'home'} to="/">Customer Home Page</Link></span> : 
+            <span className="home"><Link className={location.pathname === '/' ? 'home active' : 'home'} to="/">Home Page</Link></span>
           }
-          <span className="write"><Link className="link" to="/review">View all reviews</Link></span>
+          <span className="write"><Link className={location.pathname === '/review' ? 'link active' : 'link'} to="/review">View all reviews</Link></span>
           {!(currentUser?.isAdmin) ? 
-            <span className="schedule"><Link className="schedule" to="/schedule">My Schedule</Link></span> : 
+            <span className="schedule"><Link className={location.pathname === '/schedule' ? 'schedule active' : 'schedule'} to="/schedule">My Schedule</Link></span> : 
             null
           }
           {currentUser?.isAdmin ?
-            <span className="manage"><Link className="manage" to="/poi">Manage POIs</Link></span> : 
+            <span className="manage"><Link className={location.pathname === '/poi' ? 'manage active' : 'manage'} to="/poi">Manage POIs</Link></span> : 
             null
           }
         </div>
